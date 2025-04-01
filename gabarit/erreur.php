a11ff96b1639e256cfa0cba0077e56fb38674b49
David Marier <e2054724@cmaisonneuve.qc.ca>
2025-04-01 11:46:55 -0400

    Ajout de la couleur des boutons et de la barre de recherche

--- gabarit/erreur.php
+++ gabarit/erreur.php
@@ -3,7 +3,7 @@
     $erreur_description = get_theme_mod('erreur_description', 'Default Title');
     $erreur_background = get_theme_mod('erreur_background', 'Default Title');
     $erreur_couleur = get_theme_mod('erreur_couleur', 'Default Title');
-    $erreur_couleur2 = get_theme_mod('erreur_boutons', 'Default Title');
+    $erreur_couleur2 = get_theme_mod('erreur_bouton', '#fff200');
 ?>
 
 <section class="erreur" style="background-image: url('<?php echo $erreur_background ?>'); background-repeat: no-repeat">
@@ -14,7 +14,7 @@
         <p class="erreur__description" style="color: <?php echo $erreur_couleur ?>;">
             <?= $erreur_description ?>
         </p>
-        <a href="http://localhost:81/wordpress" class="erreur__accueil">Retour à l'accueil</a>
+        <a href="http://localhost:81/wordpress" class="erreur__accueil" style="background-color: <?php echo $erreur_couleur2 ?>;">Retour à l'accueil</a>
         <?php wp_nav_menu(array(
                     "menu"=> "erreur",
                     "container" => "nav",
@@ -25,3 +25,15 @@
         </form>
     </div>
 </section>
+
+<style>
+    #menu-erreur li{
+        background-color: <?php echo $erreur_couleur2 ?>;  
+    }
+    .recherche-erreur__input{
+        border-bottom: 2px solid <?php echo $erreur_couleur2 ?>;
+    }
+    .recherche-erreur__input::placeholder{
+        color: <?php echo $erreur_couleur2 ?>;
+    }
+</style>
