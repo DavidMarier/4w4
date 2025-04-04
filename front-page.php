b9af7ff37dba61d73261f906f255b2653650b6f9
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-04-03 20:04:15 -0400

    premier commit tp2

--- front-page.php
+++ front-page.php
@@ -27,6 +27,22 @@
             <?php endwhile; endif; ?>
         </div>
     </section>
+
+    </section>
+ 
+    <!-- ////////////////////////////////////////////////  section rest-api -->
+ 
+    <section class="destination">
+ 
+
+        <?php categories_liste("destination") ?>
+ 
+        <h2 class="destination__titre">Articles de la catégorie</h2>
+ 
+        <div class="destination__list"></div>
+ 
+    </section>
+
     <?php get_footer(); ?>
 </body>
 </html>
